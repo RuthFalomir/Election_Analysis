e398befcd7e0375d76ab7e17cc1cae4ad3cca8cc
RuthFalomir <88563922+RuthFalomir@users.noreply.github.com>
2021-10-04 15:55:24 -0500

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -70,6 +70,17 @@ with open(file_to_load) as election_data:
     #add a vote to that candidates count
         candidate_votes[candidate_name] += 1
 
+    #Save the results to our text file
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"--------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"--------------------------\n")
+    print(election_results, end="")
+    #save the final vote count to the text file
+    txt_file.write(election_results)
+
     #determine the percentage of votes for each candidate by looping thru the count
     #1. iterate thru the candidate list:
     for candidate_name in candidate_votes:
@@ -79,7 +90,12 @@ with open(file_to_load) as election_data:
         vote_percentage = float(votes) / float(total_votes) * 100
         #4. Print the candidate name & percentage votes
         #print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+         #print each canidate, their voter count and percentage to the terminal
+        print(candidate_results)
+        #save the candidate results to our text file
+        txt_file.write(candidate_results)
         #determine the winning vote count and candidate
         #1. determine if the votes are greater than the winning count
         if (votes > winning_count) and (vote_percentage > winning_percentage):
@@ -88,8 +104,8 @@ with open(file_to_load) as election_data:
             winning_percentage = vote_percentage
             #3. Set the winning_candidate = candidate_name
             winning_candidate = candidate_name
-        
-
+            
+    
     winning_candidate_summary = (
         f"---------------------------\n"
         f"Winner: {winning_candidate}\n"
@@ -98,26 +114,35 @@ with open(file_to_load) as election_data:
         f"---------------------------\n")
     print(winning_candidate_summary)
 
-print(total_votes)
-print(candidate_options)
-print(candidate_votes)
-#### Writing on file with open() & close() functions from os
-##create a filename variable to a direct or indirect path to the file
-#file_to_save = os.path.join("analysis","election_analysis,txt")
-##using the open() function with the "w" mode we will write data to the file
-#outfile = open(file_to_save,"w")
-#outfile.write("Hello World")
-#outfile.close()
+    #savew the winning candidates name to the text file
+    txt_file.write(winning_candidate_summary)
+    
 
-#### Writing on file using with statement
 
-#with open(file_to_save,"w") as txt_file:
-    #Write data to the file
-    #txt_file.write("Hello World")
-    ##first method, addding one by one
-    #txt_file.write("Arapahoe, ")
-    #txt_file.write("Denver, ")
-    #txt_file.write("Jefferson")
-    #txt_file.close()
-    #txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
-    #txt_file.write("Arapahoe\nDenver\nJefferson")
+    
+    
+   
+    
+    #print(total_votes)
+    #print(candidate_options)
+    #print(candidate_votes)
+    #### Writing on file with open() & close() functions from os
+    ##create a filename variable to a direct or indirect path to the file
+    #file_to_save = os.path.join("analysis","election_analysis,txt")
+    ##using the open() function with the "w" mode we will write data to the file
+    #outfile = open(file_to_save,"w")
+    #outfile.write("Hello World")
+    #outfile.close()
+
+    #### Writing on file using with statement
+
+    #with open(file_to_save,"w") as txt_file:
+        #Write data to the file
+        #txt_file.write("Hello World")
+        ##first method, addding one by one
+        #txt_file.write("Arapahoe, ")
+        #txt_file.write("Denver, ")
+        #txt_file.write("Jefferson")
+        #txt_file.close()
+        #txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
+        #txt_file.write("Arapahoe\nDenver\nJefferson")
